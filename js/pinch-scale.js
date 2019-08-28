12e7f175a43ba323bd30033d864632304beeba78
unknown <curt3n5@gmail.com>
2019-08-28 03:19:37 -0400

    init commit

---
+++ js/pinch-scale.js
@@ -0,0 +1,60 @@
+AFRAME.registerComponent('pinch-scale', {
+  schema: {
+    min: { default: 0.3 },
+    max: { default: 8 }
+  },
+  init: function () {
+    this.initialScale = this.el.object3D.scale.clone()
+    this.scaleFactor = 1
+    this.handleEvent = this.handleEvent.bind(this)
+    this.el.sceneEl.addEventListener('twofingermove', this.handleEvent)
+  },
+  remove: function () {
+    this.el.sceneEl.removeEventListener('twofingermove', this.handleEvent)
+  },
+  handleEvent: function (event) {
+    this.scaleFactor *= 1 + event.detail.spreadChange / event.detail.startSpread
+    this.scaleFactor = Math.min(Math.max(this.scaleFactor, this.data.min), this.data.max)
+
+    this.el.object3D.scale.x = this.scaleFactor * this.initialScale.x
+    this.el.object3D.scale.y = this.scaleFactor * this.initialScale.y
+    this.el.object3D.scale.z = this.scaleFactor * this.initialScale.z
+  }
+});
+
+AFRAME.registerComponent('pinch-scale-destroy', {
+  schema: {
+    min: { default: 0.3 },
+    max: { default: 8 }
+  },
+  init: function () {
+    this.initialScale = this.el.object3D.scale.clone()
+    this.scaleFactor = 1
+    this.handleEvent = this.handleEvent.bind(this)
+    this.el.sceneEl.addEventListener('twofingermove', this.handleEvent)
+  },
+  remove: function () {
+    this.el.sceneEl.removeEventListener('twofingermove', this.handleEvent)
+  },
+  handleEvent: function (event) {
+    this.scaleFactor *= 1 + event.detail.spreadChange / event.detail.startSpread
+    this.scaleFactor = Math.min(Math.max(this.scaleFactor, this.data.min), this.data.max)
+
+    this.el.object3D.scale.x = this.scaleFactor * this.initialScale.x
+    this.el.object3D.scale.y = this.scaleFactor * this.initialScale.y
+    this.el.object3D.scale.z = this.scaleFactor * this.initialScale.z
+
+    //if scale is below threshold display explosion animation 
+    if(this.scaleFactor < 0.5){
+      //load explosion graphic
+      const thefear = document.getElementById('thefear');
+      thefear.setAttribute('material', 'src: #explode');
+
+      //stop scary music and play explosion sound effect
+      const themusic = document.getElementById('scarymusic');
+      const explodesound = document.getElementById('explodesound');
+      themusic.pause();
+      explodesound.play();
+    }
+  }
+});
